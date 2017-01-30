40dcc75008082cd42e75dec3263f0c3481379be6
xuzj157 <xuzj157@sina.com>
2017-01-30 15:50:13 -0600

    加入欢迎界面（欢迎界面图片需要更改），美化removeActivity，调整图片大小，与间距

---
+++ app/src/main/java/personal/nekopalyer/ewhat/LaunchActivity.java
@@ -0,0 +1,39 @@
+package personal.nekopalyer.ewhat;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity {
+//持续时间比较稳定，通常用于放置广告
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(LaunchActivity.this,MainActivity.class));
+                LaunchActivity.this.finish();
+            }
+        },3000);
+
+    }
+}
+
+
+
+
+
+//public class LaunchActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_launch);
+//    }
+//}
